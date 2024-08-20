68d04df217167a5bdd291071c7aa5cc4adb49754
deepak04iiitn <dky422003@gmail.com>
2024-08-20 20:25:04 +0530

    completed the ui of My Reviews page

--- client/src/components/DashMyReviews.jsx
+++ client/src/components/DashMyReviews.jsx
@@ -1,49 +1,124 @@
 import { Button, Modal, Table } from 'flowbite-react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export default function DashMyReviews() {
+
+    const {currentUser} = useSelector(state => state.user);
+    const [showReviewsError , setShowReviewsError] = useState(false);
+    const [userReviews , setUserReviews] = useState([]);
+    const [showMore , setShowMore] = useState(true);
+
+    useEffect(() => {
+
+        const getUserReviews = async() => {
+
+            try {
+
+                setShowReviewsError(false);
+
+                const res = await fetch(`/api/user/reviews/${currentUser._id}`);
+                const data = await res.json();
+
+                if(data.success === false)
+                {
+                    setShowReviewsError(true);
+                    return;
+                }
+
+                setUserReviews(data);
+
+            } catch (error) {
+                setShowReviewsError(true);
+            }
+        }
+
+        getUserReviews();
+
+    } , [currentUser._id])
+
+
+    const handleShowMore = () => {
+
+    }
+
+
   return (
 
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
 
-                <Table hoverable className='shadow-md'>
+            {userReviews && userReviews.length > 0 ? (
+
+                <>
+                    <Table hoverable className='shadow-lg'>
 
                     <Table.Head>
                         <Table.HeadCell>Review image</Table.HeadCell>
-                        <Table.HeadCell>Review title</Table.HeadCell>
                         <Table.HeadCell>Category</Table.HeadCell>
+                        <Table.HeadCell>Review title</Table.HeadCell>
                         <Table.HeadCell>Delete</Table.HeadCell>
                         <Table.HeadCell>
                             <span>Edit</span>
                         </Table.HeadCell>
                     </Table.Head>
 
-                        <Table.Body className='divide-y'>
+                    {userReviews.map((review) => (
+
+                            <Table.Body className='divide-y'>
 
                             <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
 
                                 <Table.Cell>
+
+                                    <Link to={`/review/${review._id}`}>
+                                        <img
+                                            src={review.imageUrls[0]}
+                                            alt='review cover'
+                                            className='h-12 w-12 object-contain'
+                                        />
+                                    </Link>
                                     
                                 </Table.Cell>
 
                                 <Table.Cell>
-                                    
-                                </Table.Cell>
-
-                                <Table.Cell></Table.Cell>
-
-                                <Table.Cell>
-                                    
+                                    <Link to={`/review/${review._id}`}>
+                                        {review.category}
+                                    </Link>
                                 </Table.Cell>
 
                                 <Table.Cell>
-                                    
+                                    <Link to={`/review/${review._id}`}>
+                                        {review.subcategory}
+                                    </Link>
+                                </Table.Cell>
+
+                                <Table.Cell>
+                                    <span className='font-medium text-red-500 hover:underline cursor-pointer'>Delete</span>
+                                </Table.Cell>
+
+                                <Table.Cell>
+                                    <span>Edit</span>
                                 </Table.Cell>
                             </Table.Row>
-                        </Table.Body>
+                            </Table.Body>
+                    ))}
 
-                </Table>
+                    </Table>
+
+                    {
+                        showMore && (
+                            <button className='w-full text-teal-500 self-center text-sm py-7' onClick={handleShowMore}>
+                                Show more
+                            </button>
+                        )
+                    }
+
+                </>
+            ) : (
+                <p>You have no reviews yet!</p>
+            )} 
 
         <Modal>
 
@@ -64,6 +139,10 @@ export default function DashMyReviews() {
             </Modal.Body>
 
         </Modal>
+
+        <p className='text-red-700 mt-5'>
+            {showReviewsError ? 'Error showing reviews' : ''}
+        </p>
 
     </div>
 
